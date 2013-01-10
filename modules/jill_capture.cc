ed8b32858b7c090afab34c90589efdb6b594862b
dmeliza <dmeliza@uchicago.edu>
2013-01-09 21:40:59 -0600

    fix conflicts
    Ignore-this: 2e8b1d617368da531f3272affb2cc9e2
    
    darcs-hash:20130110034059-21d25-48d240f329ff39f003c3b7459d34a1a873520d72.gz

--- modules/jill_capture.cc
+++ modules/jill_capture.cc
@@ -12,7 +12,6 @@
  */
 #include <iostream>
 #include <signal.h>
-
 #include <boost/scoped_ptr.hpp>
 
 #include "jill/jack_client.hh"
@@ -29,19 +28,10 @@ std::map<std::string, jack_port_t*> ports_in;
 jack_port_t *port_trig;
 boost::scoped_ptr<dsp::period_ringbuffer> ringbuffer;
 
-boost::scoped_ptr<dsp::Ringbuffer<sample_t> > ringbuffer;
 /* locks and condition variables used to synchronize during buffer resize */
 // pthread_mutex_t disk_thread_lock = PTHREAD_MUTEX_INITIALIZER;
 // pthread_cond_t  data_ready = PTHREAD_COND_INITIALIZER;
 
-struct chunk_info_t {
-        std::size_t nchannels;
-        std::size_t nframes;
-};
-
-
-
-logstream logv;
 int ret = EXIT_SUCCESS;
 
 /*
