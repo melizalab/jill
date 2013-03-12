4172e1619e865edd1c32eae74ae18313bc60ea18
Dan Meliza <dan@meliza.org>
2013-03-12 13:13:51 -0500

    put offset math and queue management in jill::util::stimqueue

--- jill/util/stimset.hh
+++ jill/util/stimset.hh
@@ -50,6 +50,45 @@ private:
 
 };
 
+/**
+ * Represents a two-element stimulus queue.  One thread is pulling data off the
+ * current stimulus, while another is making sure the next stimulus is loaded.
+ * The playback thread also needs to compare the current time against the last
+ * time it played a stimulus.  The math for this is tricky, so it's included
+ * here so it can be tested.
+ */
+struct stimqueue {
+        jill::stimulus_t const * current_stim;     // currently playing stim
+        jill::stimulus_t const * next_stim;        // next stim in queue
+        size_t buffer_pos;                         // position in buffer
+        nframes_t last_start;                      // last stimulus start time
+        nframes_t last_stop;                       // last stimulus stop
+
+        /** return true if the current stimulus is loaded */
+        bool ready() const { return current_stim && current_stim->buffer(); }
+
+        /**
+         * compare the current time against the last start and stop times and
+         * determine if it's time to play the current stimulus
+         *
+         * @param time       the current sample count
+         * @param min_start  minimum time since last stimulus started
+         * @param min_stop   minimum time since last stimulus stopped
+         *
+         * @return the time to start the stimulus, relative to the current time
+         */
+
+        nframes_t offset(nframes_t time, nframes_t min_start, nframes_t min_stop) {
+                // deltas with last events - overflow is correct because time >= lastX
+                nframes_t dstart = time - last_start;
+                nframes_t dstop = time - last_stop;
+                // now we have to check for overflow
+                nframes_t ostart = (dstart > min_start) ? 0 : min_start - dstart;
+                nframes_t ostop = (dstop > min_stop) ? 0 : min_stop - dstop;
+                return std::max(ostart, ostop);
+        }
+};
+
 }} // namespace jill::util
 
 #endif
