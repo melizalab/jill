82d34ed2bf2f67f9971b67850c5cc496d334b046
Dan Meliza <dan@meliza.org>
2013-12-21 14:01:23 -0800

    tick version for 2.1 release

--- jill/version.hh
+++ jill/version.hh
@@ -1,12 +1,11 @@
 /*
  * JILL - C++ framework for JACK
  *
- * includes code from klick, Copyright (C) 2007-2009  Dominic Sacre  <dominic.sacre@gmx.de>
- * additions Copyright (C) 2010-2013 C Daniel Meliza <dan || meliza.org>
+ * Copyright (C) 2010-2013 C Daniel Meliza <dan || meliza.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  */
-#define JILL_VERSION "2.1.0-SNAPSHOT"
+#define JILL_VERSION "2.1.0"
