0f18d278a7482b760e47d102230843c86e2d04c1
melizalab <melizalab@eleuthia.lab>
2024-02-07 16:10:21 -0500

    move midi functions into separate compilation unit

--- jill/midi.hh
+++ jill/midi.hh
@@ -97,28 +97,7 @@ namespace midi {
 		data_type _value;
 	};
 
-	std::ostream& operator<< (std::ostream &os, const status_type &s) {
-		switch (s) {
-		case status_type::stim_on: os << "STIM_ON"; break;
-		case status_type::stim_off: os << "STIM_OFF"; break;
-		case status_type::info: os << "INFO"; break;
-		case status_type::note_on: os << "NOTE_ON"; break;
-		case status_type::note_off: os << "NOTE_OFF"; break;
-		case status_type::key_press: os << "KEYPRESS"; break;
-		case status_type::ctl: os << "CTL"; break;
-		case status_type::program_change: os << "PROGRAM_CHANGE"; break;
-		case status_type::channel_aftertouch: os << "AFTERTOUCH"; break;
-		case status_type::pitch_bend: os << "PITCH_BEND"; break;
-		case status_type::sysex: os << "SYSEX"; break;
-		case status_type::sysex_end: os << "SYSEX_END"; break;
-		case status_type::reset: os << "RESET"; break;
-		default: os << "UNDEFINED";
-		}
-		if (auto chan = s.channel()) {
-			os << "(" << static_cast<int>(*chan) << ")";
-		}
-		return os;
-	}
+	std::ostream& operator<< (std::ostream &os, const status_type &s);
 
         /**
          * Write a string message to a midi buffer.
@@ -129,20 +108,7 @@ namespace midi {
          * @param message  the string message, or 0 to send an empty message
          *
          */
-        int write_message(void * buffer, nframes_t time, status_type status, char const * message=nullptr) {
-                std::size_t len = 1;
-                if (message)
-                        len += strlen(message) + 1; // add the null byte
-                data_type *buf = jack_midi_event_reserve(buffer, time, len);
-                if (buf) {
-                        buf[0] = status.value();
-                        // FIXME: replace with strlcpy
-                        if (message) strcpy(reinterpret_cast<char*>(buf)+1, message);
-                        return 0;
-                }
-                else
-                        return ENOBUFS;
-        }
+        int write_message(void * buffer, nframes_t time, status_type status, char const * message=nullptr);
 
         /**
          * Find an onset or offset event in a midi event stream.
@@ -151,37 +117,12 @@ namespace midi {
          * @param onset        if true, look for onset events; if false, for offsets
          * @return the time of the first event, or -1 if no event was found.
          */
-        int find_trigger(void const * midi_buffer, bool onset=true) {
-                void * buf = const_cast<void *>(midi_buffer);
-                jack_midi_event_t event;
-                nframes_t nevents = jack_midi_get_event_count(buf);
-                for (nframes_t i = 0; i < nevents; ++i) {
-                        jack_midi_event_get(&event, buf, i);
-			if (event.size == 0)
-				continue;
-			status_type status = *reinterpret_cast<data_type const *>(event.buffer);
-                        if (onset and status.is_onset()) {
-                                return event.time;
-                        }
-                        else if (!onset and status.is_offset()) {
-                                return event.time;
-                        }
-                }
-                return -1;
-        }
+        int find_trigger(void const * midi_buffer, bool onset=true);
 
 	/** Returns true if a midi event buffer contains an onset */
-        bool is_onset(void const * midi_buffer, std::size_t size) {
-                if (size == 0) return false;
-                status_type status = *reinterpret_cast<data_type const *>(midi_buffer);
-                return status.is_onset();
-        }
+        bool is_onset(void const * midi_buffer, std::size_t size);
 
-        bool is_offset(void const * midi_buffer, std::size_t size) {
-                if (size == 0) return false;
-                status_type status = *reinterpret_cast<data_type const *>(midi_buffer);
-                return status.is_offset();
-        }
+        bool is_offset(void const * midi_buffer, std::size_t size);
 
 } } // jill::midi
 
